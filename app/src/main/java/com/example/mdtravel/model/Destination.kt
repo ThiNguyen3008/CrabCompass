0d6933c642858c96e018f29c68eca6d8687fb8da
Ethan Glickman <128056135+eglickman@users.noreply.github.com>
2025-12-06 00:08:41 -0500

    Firebase additions
    Added default values for all fields and included an ID field for Firestore integration.

--- app/src/main/java/com/example/mdtravel/model/Destination.kt
+++ app/src/main/java/com/example/mdtravel/model/Destination.kt
@@ -5,15 +5,19 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Destination(
-    val name: String,
-    val city: String,
-    val price: Int,
-    val season: String,
-    val interest: String,
-    val description: String,
-    val link: String,
-    val lat: Double,
-    val lng: Double,
-    var rating: Double,
-    var ratingCount: Int
+    // Added this ID field to track the Firestore Document ID
+    var id: String = "",
+
+    // All fields must have default values for Firebase to work
+    val name: String = "",
+    val city: String = "",
+    val price: Int = 0,
+    val season: String = "",
+    val interest: String = "",
+    val description: String = "",
+    val link: String = "",
+    val lat: Double = 0.0,
+    val lng: Double = 0.0,
+    var rating: Double = 0.0,
+    var ratingCount: Int = 0
 ) : Parcelable
